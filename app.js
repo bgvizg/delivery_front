5ff0335d0a6209eaa7117bfd4847a42e66d18e7a
bgvizg <bgvizg9uz2hh9881@gmail.com>
2026-01-10 10:55:55 +0900

    .

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const API_BASE = "https://remains-counting-say-perception.trycloudflare.com";
 
 let map;
 let markers = [];
+let infoWindow = null;
 
 async function loadAreas() {
   const res = await fetch(`${API_BASE}/areas`);
   const areas = await res.json();
+
   const select = document.getElementById("areaSelect");
+  select.innerHTML = "";
+
   areas.forEach((a) => {
     const opt = document.createElement("option");
     opt.value = a;
@@ -27,23 +31,29 @@ async function loadDeliveries(area) {
   json.data.forEach((item) => {
     const [rank, addr, lat, lon, memo] = item;
 
-    const marker = new naver.maps.Marker({
-      position: new naver.maps.LatLng(lat, lon),
+    const position = new kakao.maps.LatLng(lat, lon);
+
+    const marker = new kakao.maps.Marker({
+      position,
       map,
     });
 
-    const info = new naver.maps.InfoWindow({
-      content: `
-        <div style="padding:8px">
-          <b>순위:</b> ${rank}<br/>
-          <b>주소:</b> ${addr}<br/>
-          <b>메모:</b> ${memo}
-        </div>
-      `,
-    });
+    const content = `
+      <div style="padding:8px;font-size:13px;">
+        <b>순위:</b> ${rank}<br/>
+        <b>주소:</b> ${addr}<br/>
+        <b>메모:</b> ${memo}
+      </div>
+    `;
 
-    naver.maps.Event.addListener(marker, "click", () => {
-      info.open(map, marker);
+    kakao.maps.event.addListener(marker, "click", () => {
+      if (infoWindow) infoWindow.close();
+
+      infoWindow = new kakao.maps.InfoWindow({
+        content,
+      });
+
+      infoWindow.open(map, marker);
     });
 
     markers.push(marker);
@@ -56,12 +66,16 @@ function clearMarkers() {
 }
 
 function initMap() {
-  map = new naver.maps.Map("map", {
-    center: new naver.maps.LatLng(37.5665, 126.978),
-    zoom: 10,
-  });
+  const container = document.getElementById("map");
+
+  const options = {
+    center: new kakao.maps.LatLng(37.5665, 126.978), // 서울
+    level: 5,
+  };
+
+  map = new kakao.maps.Map(container, options);
 
   loadAreas();
 }
 
-initMap();
+window.onload = initMap;
